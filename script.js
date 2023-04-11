395420dea86b4b40cde1850e2c4e90d993ed8529
etiry <7997357+etiry@users.noreply.github.com>
2023-04-11 17:58:39 -0400

    Add UI

--- script.js
+++ script.js
@@ -4,67 +4,92 @@ function getComputerChoice() {
 	return computerChoice = options[Math.floor(Math.random() * options.length)];
 }
 
+const container = document.querySelector('#container')
+const result = document.createElement('p');
+const runningScore = document.createElement('p');
+
+
 // compare player selection with computer selection
-function playRound(playerSelection, computerSelection) {
-	if (playerSelection.toLowerCase() === 'rock') {
+function playRound(e) {
+
+	let computerSelection = getComputerChoice();
+	let roundResult = '';
+
+	if (e.srcElement.id === 'rock') {
 		if (computerSelection === 'Paper') {
-			console.log('You lose! Paper beats Rock');
-			return 'loss';
+			result.textContent = 'You lose! Paper beats Rock';
+			container.appendChild(result);
+			roundResult = 'loss';
 		} else if (computerSelection === 'Scissors') {
-			console.log('You win! Rock beats scissors');
-			return 'win';
+			result.textContent = 'You win! Rock beats scissors';
+			container.appendChild(result);
+			roundResult = 'win';
 		} else {
-			console.log('It\'s a tie!');
+			result.textContent = 'It\'s a tie!';
+			container.appendChild(result);
 		}
-	} else if (playerSelection.toLowerCase() === 'scissors') {
+	} else if (e.srcElement.id === 'scissors') {
 		if (computerSelection === 'Rock') {
-			console.log('You lose! Rock beats scissors');
-			return 'loss';
+			result.textContent = 'You lose! Rock beats scissors';
+			container.appendChild(result);
+			roundResult = 'loss';
 		} else if (computerSelection === 'Paper') {
-			console.log('You win! Scissors beats paper');
-			return 'win'
+			result.textContent = 'You win! Scissors beats paper';
+			container.appendChild(result);
+			roundResult = 'win';
 		} else {
-			console.log('It\'s a tie!');
+			result.textContent = 'It\'s a tie!';
+			container.appendChild(result);
 		}
-	} else if (playerSelection.toLowerCase() === 'paper') {
+	} else if (e.srcElement.id === 'paper') {
 		if (computerSelection === 'Rock') {
-			console.log('You win! Paper beats rock');
-			return 'win'
+			result.textContent = 'You win! Paper beats rock';
+			container.appendChild(result);
+			roundResult = 'win';
 		} else if (computerSelection === 'Scissors') {
-			console.log('You lose! Scissors beats paper');
-			return 'loss'
+			result.textContent = 'You lose! Scissors beats paper';
+			container.appendChild(result);
+			roundResult = 'loss';
 		} else {
-			console.log('It\'s a tie!');
+			result.textContent = 'It\'s a tie!';
+			container.appendChild(result);
 		}
-	} else {
-		return 'Please enter a rock, paper, or scissors';
 	}
+
+	runningScore.textContent = calculateScore(roundResult);
+	container.appendChild(runningScore);
 }
 
-// play 5 rounds and output final score
-function game() {
+let playerScore = 0;
+let computerScore = 0;
 
-	let playerScore = 0;
-	let computerScore = 0;
+// play until someone accrues 5 points and output final score
+function calculateScore(roundResult) {
 
-	for (let i = 0; i < 5; i++) {
-		let playerChoice = prompt("Choose rock, paper, or scissors and enter your choice here");
-
-		roundResult = playRound(playerChoice, getComputerChoice());
+	while (playerScore <= 4 && computerScore <= 4) {
 
 		if (roundResult === 'loss') {
 			computerScore += 1;
 		} else if (roundResult === 'win') {
 			playerScore += 1;
-			}
+		}
+
+		return `Your score: ${playerScore}\nComputer score: ${computerScore}`;
 	}
 
+	buttons.forEach(button => button.removeEventListener('click', playRound));
 
 	if (playerScore > computerScore) {
 		return `You win the game! ${playerScore} rounds vs. ${computerScore} rounds`;
-	} else if (computerScore > playerScore) {
-		return `You lose the game! ${playerScore} rounds vs. ${computerScore} rounds`;
 	} else {
-		return `It's a tie! ${playerScore} rounds vs. ${computerScore} rounds`;
-	}
+		return `You lose the game! ${playerScore} rounds vs. ${computerScore} rounds`;
+	} 
+
 }
+
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', playRound));
